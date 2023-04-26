4efb7b5062a72c3d90a612b0c704dd5040081b3b
menghuiguli <fujia0@qq.com>
2023-04-26 22:34:56 +0800

    string

--- test2.c
+++ test2.c
@@ -1,26 +1,26 @@
 #include "stdio.h"
 
-int main(){
-    int a[100];
-    int n;
+int main() {
+  int a[100];
+  int n;
 
-    //
-    //put 10 nums to array
-    printf("please input n value:");
-    scanf("%d",&n);
-    printf("the n is %d", n);
-    for(int i=0;i<n;i++){
-        scanf("%d", &a[i]);
-    }
+  //
+  // put 10 nums to array
+  printf("please input n value:");
+  scanf("%d", &n);
+  printf("the n is %d", n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &a[i]);
+  }
 
-    printf("the reverse sort is \n");
-    for(int i=n-1;i>=0;i--){
-        printf("%d ", a[i]);
-    }
+  printf("the reverse sort is \n");
+  for (int i = n - 1; i >= 0; i--) {
+    printf("%d ", a[i]);
+  }
 
-    printf("\n\nthe regular sort is:\n");
-    for(int i=0;i<n;i++){
-        printf("%d ", a[i]);
-    }
-    return 0;
+  printf("\n\nthe regular sort is:\n");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", a[i]);
+  }
+  return 0;
 }
---
+++ test5.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+
+int main() {
+  char *s;
+  s = "ss";
+  printf("hello world!");
+  return 0;
+}
